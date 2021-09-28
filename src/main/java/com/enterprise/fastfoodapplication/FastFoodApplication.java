cc8c86f51569c18d83d5edb3ea6b1b6ee87290b2
Bipal Goyal <goyalbl@mail.uc.edu>
2021-09-28 20:07:27 +0545

    Added basic spring boot application

---
+++ src/main/java/com/enterprise/fastfoodapplication/FastFoodApplication.java
@@ -0,0 +1,13 @@
+package com.enterprise.fastfoodapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastFoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastFoodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/enterprise/fastfoodapplication/FastFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enterprise.fastfoodapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastFoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
