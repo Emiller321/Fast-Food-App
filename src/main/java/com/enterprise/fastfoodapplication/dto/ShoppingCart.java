ea84ecd02d12233cce31021d18cd2f77c453df54
Bipal Goyal <goyalbl@mail.uc.edu>
2021-09-30 02:22:21 +0545

    Added relevant methods for functions needed in a ShoppingCart

--- src/main/java/com/enterprise/fastfoodapplication/dto/ShoppingCart.java
+++ src/main/java/com/enterprise/fastfoodapplication/dto/ShoppingCart.java
@@ -4,10 +4,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ShoppingCart {
-    private String shoppingId;
-    private Map<String, CartOrder> orders;
 
-    public   ShoppingCart() {
+    /** We might not need this to be honest, I can't think of adding this to our database
+     * and don't see a reason as to why we'd need this id honestly
+     */
+    private String shoppingId;
+    private Map<String, CartOrder> orders; //We can probably use foodId as key in this Map object
+
+    public ShoppingCart() {
         orders = new HashMap<String, CartOrder>();
     }
 
@@ -16,7 +20,8 @@ public class ShoppingCart {
         orders = new HashMap<String, CartOrder>();
     }
 
-    public double totalEstimatedCostOfEntireCart() {
+    public double totalEstimatedCostOfEntireCart()
+    {
         double estimatedTotal = 0;
         for (CartOrder order : orders.values()) {
             estimatedTotal += order.getFood().getFoodPrice()*order.getFoodQuantity();
@@ -24,5 +29,35 @@ public class ShoppingCart {
         return estimatedTotal;
     }
 
+    /**
+     * Returns details of a order for a given cart based on its foodId
+     * @param foodId a unique identifier to represent a Food object
+     * @return a CartOrder object which contains food details and its quantity for this cart
+     */
+    public CartOrder getOrderDetailsInCart(String foodId) {
+        //TO DO Implementation
+        return null;
+    }
 
+    /**
+     * To add or update an order to your cart. If the food item exists, then update quantity. Otherwise, add
+     * a new order in the orders HashMap
+     * @param order takes a CartOrder object containing food information and its quantity
+     */
+    public void addOrUpdateOrderToCart(CartOrder order) {
+        String cartOrderFoodId=order.getFood().getFoodId();
+        if(orders.containsKey(cartOrderFoodId)){
+            CartOrder currentOrder=orders.get(cartOrderFoodId);
+            currentOrder.setFoodQuantity(currentOrder.getFoodQuantity()+order.getFoodQuantity());
+            orders.put(cartOrderFoodId,currentOrder);
+        }
+        else{
+            orders.put(cartOrderFoodId, order);
+        }
+    }
+
+    public void removeOrderFromCart (CartOrder order) {
+        String cartOrderFoodId=order.getFood().getFoodId();
+        orders.remove(cartOrderFoodId);
+    }
 }
