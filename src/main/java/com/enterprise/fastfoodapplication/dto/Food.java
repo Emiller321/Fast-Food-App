49f14839b2770bbfbc01f3ff986ba75656d63ff3
Bipal Goyal <goyalbl@mail.uc.edu>
2021-09-29 23:49:57 +0545

    Added Food DTO

---
+++ src/main/java/com/enterprise/fastfoodapplication/dto/Food.java
@@ -0,0 +1,16 @@
+package com.enterprise.fastfoodapplication.dto;
+
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+public @Data
+class Food {
+    private String foodId;
+    private String foodName;
+    private String foodCategory;
+    private String foodDescription;
+    private double foodPrice;
+    private String foodServing;
+    private String foodDetails;
+    private MultipartFile foodImage;
+}
