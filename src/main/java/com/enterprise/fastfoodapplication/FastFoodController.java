c0b7db3d63d411e9e9ea549ed6a58c68f87cf95c
Jubn-yan <jubn@mail.uc.edu>
2021-10-02 20:22:03 +0800

    create interface for page recent order and navigation bar
    add a new class OrderHistory; import thymeleaf and bootstrap to the start.html; add icon to the navigation bar;add food pictures to the shopping cart ;Create UI for search bar; Create UI for page order history.

--- src/main/java/com/enterprise/fastfoodapplication/FastFoodController.java
+++ src/main/java/com/enterprise/fastfoodapplication/FastFoodController.java
@@ -1,12 +1,40 @@
 package com.enterprise.fastfoodapplication;
 
+import com.enterprise.fastfoodapplication.dto.CartOrder;
+import com.enterprise.fastfoodapplication.dto.Food;
+import com.enterprise.fastfoodapplication.dto.OrderHistory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FastFoodController {
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model) {
+        /**
+         * Need an orderHistory class to store the name and number of Items a customer ordered.
+         * This happens when the user click "check out".
+         * The "check out" button act like a "save" button to store the information of order in OrderHistory class.
+         * */
+        OrderHistory orderHistory= new OrderHistory();
+        orderHistory.setFoodAmount(2);
+        orderHistory.setFoodName("burger");
+        model.addAttribute(orderHistory);
         return "start";
     }
+    /**
+     * This is for search bar on the navigation bar.
+     * This happens when the user click "search".
+     * */
+    @GetMapping("/Food")
+    public ResponseEntity searchFood(@RequestParam(value="searchTerm",required = false,defaultValue = "None") String searchTerm){
+        String newSearchTerm = searchTerm + "";
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+
 }
---
+++ src/main/java/com/enterprise/fastfoodapplication/dto/OrderHistory.java
@@ -0,0 +1,9 @@
+package com.enterprise.fastfoodapplication.dto;
+
+import lombok.Data;
+
+public @Data
+ class OrderHistory {
+    private String foodName;
+    private int foodAmount;
+}
